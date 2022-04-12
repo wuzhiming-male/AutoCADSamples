deb083bccf65be947d54b85d7fc314616d973176
wuzhiming-male <411425679@qq.com>
2022-04-12 14:34:52 +0800

    动态绘制

---
+++ AutoCADSamples/创建图元实体/动态绘制/DrawJigDemo.cs
@@ -0,0 +1,112 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.GraphicsInterface;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samples.动态绘制
+{
+    public class DrawJigDemo : DrawJig
+    {
+        public List<Entity> _Entities = new List<Entity>();
+        private Point3d _BasePoint;
+        private Point3d _MovePoint;
+        public DrawJigDemo(Point3d basePoint)
+        {
+            _BasePoint = basePoint;
+        }
+        protected override SamplerStatus Sampler(JigPrompts prompts)
+        {
+            JigPromptPointOptions jppo = new JigPromptPointOptions("\n指定移动点：");
+            jppo.UseBasePoint = true;
+            jppo.BasePoint = _BasePoint;
+
+
+            PromptPointResult ppr = prompts.AcquirePoint(jppo);
+            if (ppr.Status == PromptStatus.OK && ppr.Value.DistanceTo(_BasePoint) > 10e-10)
+            {
+                _MovePoint = ppr.Value;
+                return SamplerStatus.OK;
+            }
+            else
+            {
+                return SamplerStatus.Cancel;
+            }
+        }
+
+        protected override bool WorldDraw(WorldDraw draw)
+        {
+            Matrix3d mat = Matrix3d.Displacement(_BasePoint.GetVectorTo(_MovePoint));
+            WorldGeometry geo = draw.Geometry;
+            if(geo != null)
+            {
+                geo.PushModelTransform(mat);
+                foreach (Entity ent in _Entities)
+                    geo.Draw(ent);
+                geo.PopModelTransform();
+                return true;
+            }
+            return false;
+        }
+
+        public void AddEntity(Entity ent)
+        {
+            _Entities.Add(ent);
+        }
+
+        public void TransformEntities()
+        {
+            Matrix3d mat = Matrix3d.Displacement(_BasePoint.GetVectorTo(_MovePoint));
+            foreach(Entity ent in _Entities)
+            {
+                ent.TransformBy(mat);
+            }
+        }
+        public static void Jig()
+        {
+            //平移实体动态显示
+            Editor ed = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Editor;
+
+            PromptSelectionResult psr = ed.GetSelection();
+            if (psr.Status != PromptStatus.OK)
+                return;
+
+            PromptPointResult ppr = ed.GetPoint("\n选择基点");
+            if (ppr.Status != PromptStatus.OK)
+                return;
+
+            Database db = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Database;
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                DrawJigDemo dJig = new DrawJigDemo(ppr.Value);
+                foreach (ObjectId oid in psr.Value.GetObjectIds())
+                {
+                    Entity ent = tr.GetObject(oid, OpenMode.ForWrite) as Entity;
+                    dJig.AddEntity(ent);
+                }
+                PromptResult pr = ed.Drag(dJig);
+                if (pr.Status == PromptStatus.OK)
+                {
+                    dJig.TransformEntities();
+                    tr.Commit();
+                }
+                else
+                {
+                    tr.Abort();
+                }
+
+            }
+
+        }
+        [CommandMethod("tsDrawJig")]
+        public static void tsDrawJig()
+        {
+            DrawJigDemo.Jig();
+        }
+    }
+}
---
+++ AutoCADSamples/创建图元实体/动态绘制/EntityJigDemo.cs
@@ -0,0 +1,94 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samples.动态绘制
+{
+    public class EntityJigDemo : EntityJig
+    {
+        private Circle _Circle;
+        public double R;
+        public EntityJigDemo(Circle c) : base(c)
+        {
+            _Circle = c;
+        }
+        protected override SamplerStatus Sampler(JigPrompts prompts)
+        {
+            JigPromptPointOptions jppo = new JigPromptPointOptions();
+            jppo.UseBasePoint = true;
+            jppo.BasePoint = _Circle.Center;
+
+            PromptPointResult ppr = prompts.AcquirePoint(jppo);
+            if (ppr.Status == PromptStatus.OK && ppr.Value.DistanceTo(_Circle.Center) > 10e-10)
+            {
+                R = ppr.Value.DistanceTo(_Circle.Center);
+                return SamplerStatus.OK;
+            }
+            else if (ppr.Value.DistanceTo(_Circle.Center) < 10e-10)
+                return SamplerStatus.NoChange;
+            else
+                return SamplerStatus.Cancel;
+        }
+
+        protected override bool Update()
+        {
+            try
+            {
+                _Circle.Radius = R;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static void Jig()
+        {
+            //模拟动态绘制圆
+            Editor ed = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Editor;
+            Database db = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Database;
+            PromptPointResult ppr = ed.GetPoint("\n选择圆心");
+            if(ppr.Status != PromptStatus.OK)
+            {
+                return;
+            }
+
+            using(Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                BlockTable bt = tr.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                BlockTableRecord btr = tr.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+
+                Circle c = new Circle();
+                c.Center = ppr.Value;
+                c.Radius = 1.0;
+                btr.AppendEntity(c);
+                tr.AddNewlyCreatedDBObject(c, true);
+                EntityJigDemo jig = new EntityJigDemo(c);
+                PromptResult pr = ed.Drag(jig);
+                if(pr.Status == PromptStatus.OK)
+                {
+                    c.Radius = jig.R;
+                    tr.Commit();
+                }
+                else
+                {
+                    c.Dispose();
+                    tr.Abort();
+                }
+
+            }
+            
+        }
+        [CommandMethod("tsEntityJig")]
+        public static void tsEntityJig()
+        {
+            EntityJigDemo.Jig();
+        }
+    }
+}
