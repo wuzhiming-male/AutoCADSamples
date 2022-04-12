651de1d29676af2e1fd6bb2878a8ef6c9c6f24f4
wuzhiming <411425679@qq.com>
2022-04-12 09:05:11 +0800

    CAD案例代码

---
+++ AutoCADSamples/创建图元实体/创建实体.cs
@@ -0,0 +1,175 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samples
+{
+    public class 创建实体
+    {
+        [CommandMethod("AddPolyline")]
+        public void Polyline_多段线()
+        {
+            Database db = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Database;
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                BlockTable bt = tr.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                BlockTableRecord btr = tr.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+
+                Polyline pl = new Polyline();
+                pl.AddVertexAt(0, new Point2d(0, 0), 0, 0, 0);
+                pl.AddVertexAt(1, new Point2d(10, 10), 0, 0, 0);
+                pl.AddVertexAt(2, new Point2d(100, 50), 0, 0, 0);
+
+                btr.AppendEntity(pl);
+                tr.AddNewlyCreatedDBObject(pl, true);
+                tr.Commit();
+            }
+        }
+
+        [CommandMethod("AddLine")]
+        public void Line_直线()
+        {
+            Database db = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Database;
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                BlockTable bt = tr.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                BlockTableRecord btr = tr.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+
+                Line line = new Line();
+                line.StartPoint = new Point3d(0, 0, 0);
+                line.EndPoint = new Point3d(10, 10, 0);
+
+                btr.AppendEntity(line);
+                tr.AddNewlyCreatedDBObject(line, true);
+                tr.Commit();
+            }
+        }
+
+        [CommandMethod("AddText")]
+        public void DBText_文字注记()
+        {
+            Database db = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Database;
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                BlockTable bt = tr.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                BlockTableRecord btr = tr.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+
+                DBText dbtext = new DBText();
+                dbtext.TextString = "hello world";
+                dbtext.Height = 10;
+                dbtext.HorizontalMode = TextHorizontalMode.TextCenter;//水平方向居中
+                dbtext.VerticalMode = TextVerticalMode.TextVerticalMid;//水平方向居中
+                dbtext.Position = new Point3d(100, 100, 0);
+                //当手动设置了文字的居中方式时,一定要设置AlignmentPoint属性，否则文字显示在原点
+                dbtext.AlignmentPoint = dbtext.Position;
+
+                btr.AppendEntity(dbtext);
+                tr.AddNewlyCreatedDBObject(dbtext, true);
+                tr.Commit();
+            }
+        }
+
+        [CommandMethod("AddCircle")]
+        public void Circle_圆()
+        {
+            Database db = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Database;
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                BlockTable bt = tr.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                BlockTableRecord btr = tr.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+
+                Circle c = new Circle();
+                c.Center = new Point3d(0, 0, 0);
+                c.Radius = 100;
+
+                btr.AppendEntity(c);
+                tr.AddNewlyCreatedDBObject(c, true);
+                tr.Commit();
+            }
+        }
+        [CommandMethod("AddXline")]
+        public void Xline_构造线()
+        {
+            Database db = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Database;
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                BlockTable bt = tr.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                BlockTableRecord btr = tr.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+
+                Xline xline = new Xline();
+                xline.BasePoint = new Point3d(0, 0, 0);
+                xline.SecondPoint = new Point3d(10, 10, 0);
+
+                btr.AppendEntity(xline);
+                tr.AddNewlyCreatedDBObject(xline, true);
+                tr.Commit();
+            }
+        }
+
+        [CommandMethod("AddEllipse")]
+        public void Ellipse_椭圆()
+        {
+            Database db = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Database;
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                BlockTable bt = tr.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                BlockTableRecord btr = tr.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+
+                //RadiusRatio属性必须小于1
+                Ellipse e = new Ellipse(new Point3d(0, 0, 0), Vector3d.ZAxis, Vector3d.XAxis, 0.5, 0, 2 * Math.PI);
+               
+
+                btr.AppendEntity(e);
+                tr.AddNewlyCreatedDBObject(e, true);
+                tr.Commit();
+            }
+        }
+        [CommandMethod("AddRegion")]
+        public void Region_面域()
+        {
+            Database db = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Database;
+            Editor ed = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Editor;
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                PromptEntityResult per = ed.GetEntity("选择实体");
+                if (per.Status != PromptStatus.OK)
+                    return;
+
+                Entity ent = tr.GetObject(per.ObjectId, OpenMode.ForWrite) as Entity;
+
+                BlockTable bt = tr.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                BlockTableRecord btr = tr.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+
+                DBObjectCollection dBObjectCollection = new DBObjectCollection();
+                dBObjectCollection.Add(ent);
+                Region region = Region.CreateFromCurves(dBObjectCollection)[0] as Region;
+
+                btr.AppendEntity(region);
+                tr.AddNewlyCreatedDBObject(region, true);
+                tr.Commit();
+            }
+        }
+
+        [CommandMethod("entityType")]
+        public void entityType()
+        {
+            Database db = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Database;
+            Editor ed = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Editor;
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                PromptEntityResult per = ed.GetEntity("选择实体");
+                if (per.Status != PromptStatus.OK)
+                    return;
+
+                Entity ent = tr.GetObject(per.ObjectId, OpenMode.ForWrite) as Entity;
+                ed.WriteMessage($"\n{ent.GetType().Name}");
+            }
+        }
+    }
+}
---
+++ AutoCADSamples/创建图元实体/动态绘制.cs
@@ -0,0 +1,201 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.GraphicsInterface;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Polyline = Autodesk.AutoCAD.DatabaseServices.Polyline;
+
+namespace Samples
+{
+    public class 动态绘制
+    {
+        [CommandMethod("tsDrawJig")]
+        public void tsDrawJig()
+        {
+            TextDrawJig.Jig();
+        }
+    }
+
+    public class MyDragJig : DrawJig
+    {
+        private Point3d _centerPoint;
+        private double _Radius;
+        private Circle _circle;
+        public double Radius { get => _Radius; }
+        public MyDragJig(Point3d centerPoint, Circle circle)
+        {
+            _centerPoint = centerPoint;
+            _circle = circle;
+        }
+        protected override SamplerStatus Sampler(JigPrompts prompts)
+        {
+            JigPromptPointOptions jppo = new JigPromptPointOptions("\n指定半径：");
+            jppo.UseBasePoint = true;
+            jppo.BasePoint = _centerPoint;
+            
+
+            PromptPointResult ppr = prompts.AcquirePoint(jppo);
+            if(ppr.Status == PromptStatus.OK && ppr.Value.DistanceTo(_centerPoint) > 10e-10)
+            {
+                _Radius = ppr.Value.DistanceTo(_centerPoint);
+                return SamplerStatus.OK;
+            }
+            else
+            {
+                return SamplerStatus.Cancel;
+            }
+
+        }
+
+        protected override bool WorldDraw(WorldDraw draw)
+        {
+            _circle.Radius = _Radius;
+            _circle.WorldDraw(draw);
+            return true;
+        }
+
+        public static void Jig()
+        {
+            Editor ed = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Editor;
+            //PromptPointResult ppr = ed.GetPoint("选择圆心");
+            //if (ppr.Status != PromptStatus.OK)
+            //    return;
+
+            PromptEntityResult per = ed.GetEntity("选择圆");
+            if (per.Status != PromptStatus.OK)
+                return;
+
+            Database db = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Database;
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                Circle c = tr.GetObject(per.ObjectId, OpenMode.ForWrite) as Circle;
+                MyDragJig dragJig = new MyDragJig(c.Center, c);
+                PromptResult pr = ed.Drag(dragJig);
+                if(pr.Status == PromptStatus.OK)
+                {
+                    c.Radius = dragJig.Radius;
+                    tr.Commit();
+                }
+                else
+                {
+                    tr.Abort();
+                }
+
+            }
+
+        }
+    }
+    public class TextDrawJig : DrawJig
+    {
+        private DBText _Text;
+        public Point3d InsertPoint;
+        private List<Entity> entities = new List<Entity>();
+        public TextDrawJig(DBText dBText)
+        {
+            _Text = dBText;
+        }
+        protected override SamplerStatus Sampler(JigPrompts prompts)
+        {
+            JigPromptPointOptions jppo = new JigPromptPointOptions("\n选择插入点");
+            PromptPointResult ppr = prompts.AcquirePoint(jppo);
+            if(ppr.Status == PromptStatus.OK)
+            {
+                InsertPoint = ppr.Value;
+                return SamplerStatus.OK;
+            }
+            else
+            {
+                return SamplerStatus.Cancel;
+            }
+        }
+
+        protected override bool WorldDraw(WorldDraw draw)
+        {
+            
+            Matrix3d mat = Matrix3d.Displacement(_Text.Position.GetVectorTo(InsertPoint));
+            WorldGeometry geo = draw.Geometry;
+            if (geo != null)
+            {
+                geo.PushModelTransform(mat);
+                geo.Draw(_Text);
+                foreach (Entity ent in entities)
+                    geo.Draw(ent);
+                geo.PopModelTransform();
+                return true;
+            }
+            return false;
+        }
+
+        public void AddEnt(Entity ent)
+        {
+            entities.Add(ent);
+        }
+        public void TransformEntities()
+        {
+            Matrix3d mat = Matrix3d.Displacement(_Text.Position.GetVectorTo(InsertPoint));
+            foreach (Entity ent in entities)
+            {
+                ent.TransformBy(mat);
+            }
+
+        }
+        public static void Jig()
+        {
+            Editor ed = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Editor;
+            Database db = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Database;
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                BlockTable bt = tr.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                BlockTableRecord btr = tr.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+                DBText dBText = new DBText();
+                dBText.TextString = "hello word";
+                dBText.Height = 10;
+                dBText.Position = new Point3d(0, 0, 0);
+                btr.AppendEntity(dBText);
+                tr.AddNewlyCreatedDBObject(dBText, true);
+                Polyline pl = new Autodesk.AutoCAD.DatabaseServices.Polyline();
+                pl.AddVertexAt(0, new Point2d(0, 0), 0, 0, 0);
+                pl.AddVertexAt(1, new Point2d(0, 20), 0, 0, 0);
+                btr.AppendEntity(pl);
+                tr.AddNewlyCreatedDBObject(pl, true);
+
+                TextDrawJig dragJig = new TextDrawJig(dBText);
+                dragJig.AddEnt(pl);
+
+                for(int i = 0; i < 10; ++i)
+                {
+                    for(int j = 0; j < 10; ++j)
+                    {
+                        DBText dBTextTmp = new DBText();
+                        dBTextTmp.TextString = "hello word";
+                        dBTextTmp.Height = 1;
+                        dBTextTmp.Position = new Point3d(10 * i + 10 * j, 10 * i, 0);
+                        btr.AppendEntity(dBTextTmp);
+                        tr.AddNewlyCreatedDBObject(dBTextTmp, true);
+                        dragJig.AddEnt(dBTextTmp);
+
+                    }
+                   
+                }
+                PromptResult pr = ed.Drag(dragJig);
+                if (pr.Status == PromptStatus.OK)
+                {
+                    dragJig.TransformEntities();
+                    dBText.Position = dragJig.InsertPoint;
+                    tr.Commit();
+                }
+                else
+                {
+                    tr.Abort();
+                }
+
+            }
+
+        }
+    }
+}
